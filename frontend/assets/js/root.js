6c54599f01140ebf1b24705a68f2aa3960ec89a2
arij chebbi <arij.chebbi@insat.ucar.tn>
2026-03-27 16:19:42 +0100

    added new html pages to work on

--- frontend/assets/js/root.js
+++ frontend/assets/js/root.js
@@ -1,14 +1,12 @@
+//lel hajet standard kima lmode chabeb
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("themeBtn");
-
-    // APPLY SAVED THEME
     const saved = localStorage.getItem("theme");
     if (saved === "dark") {
         document.documentElement.setAttribute("data-theme", "dark");
         if (btn) btn.innerHTML = '<i class="fa-solid fa-moon"></i>';
     }
 
-    // CLICK EVENT
     if (btn) {
         btn.onclick = toggleTheme;
     }
