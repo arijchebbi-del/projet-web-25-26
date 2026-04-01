1e1ec10e2542dd3aa0b5c7ebcae2a9f0d194fee0
arij chebbi <arij.chebbi@insat.ucar.tn>
2026-04-01 23:10:24 +0100

    data base connexion tested ! (it works '-')

--- backend/config/database.php
+++ backend/config/database.php
@@ -7,7 +7,6 @@ $password = "";
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !"; 
 } catch (PDOException $e) {
     die("Erreur DB: " . $e->getMessage());
 }
