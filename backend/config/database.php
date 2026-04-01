4ae737733ac0abdedd8ea57ecdc11b0c6668ab04
arij chebbi <arij.chebbi@insat.ucar.tn>
2026-04-01 23:09:32 +0100

    database connected

---
+++ backend/config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = "127.0.0.1:3307";
+$dbname = "alumini"; 
+$username = "root";
+$password = "";   
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !"; 
+} catch (PDOException $e) {
+    die("Erreur DB: " . $e->getMessage());
+}
+?>
